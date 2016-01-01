7ccfcc728ffee036dc9e9b5909fdaf38e5f5a3fb
lukaskwkw <lukaskwkw@gmail.com>
2016-01-01 18:34:08 +0100

    basic setup for ms. To-do: connect mongodb, shortener function

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -6,7 +6,6 @@
    var deleteButton = document.querySelector('.btn-delete');
    var clickNbr = document.querySelector('#click-nbr');
    var apiUrl = 'http://localhost:3000/api/clicks';
-
    function ready (fn) {
       if (typeof fn !== 'function') {
          return;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,43 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+var re_weburl = require('../../regex-weburl.js');
+var http = require("http");
+var url = require("url");
+//0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+//
+
+// var newHandler = function (link,localRes,db)
+
+function testUrl (link, localRes) {
+	var hostName = url.parse(link).hostname;
+	var options = {
+		method: 'HEAD',
+		hostname: hostName,
+		port: 80
+	}
+	http.request(options, function() {
+		localRes.end(JSON.stringify({original_url : link, short_url: 'to do'}))
+
+	}).on('error', function(e) {
+		e.hostname = hostName;
+		e.port = 80;
+  localRes.end(JSON.stringify(e))
+}).end();
+
+}
+
+function validateUrl (link, localRes) {
+	if (/https?:\/\//i.test(link)===false)
+		link = "http://" + link;
+	if (re_weburl.test(link)) 
+		return testUrl(link,localRes);
+	else
+		return localRes.end("invalid url");
+}
 
 module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
 
-   app.route('/')
-      .get(function (req, res) {
-         res.sendFile(process.cwd() + '/public/index.html');
-      });
-
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+   app.get('/new/*', function  (req,res) {
+   	validateUrl(req.params[0],res);
+   })
 };
